9f8c2d317446c1f4467a5882905a7397a2c69ea8
JuanAddiel <addieljaquez@gmail.com>
2024-04-07 11:46:37 -0400

    Pages add

--- src/components/AddEvent.tsx
+++ src/components/AddEvent.tsx
@@ -1,36 +1,129 @@
-import { IonButton, IonButtons, IonContent, IonHeader, IonInput, IonItem, IonModal, IonTitle, IonToolbar } from "@ionic/react";
+import React, { useState } from "react";
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonModal,
+  IonTextarea,
+  IonImg,
+  IonItemDivider,
+  IonDatetime,
+  IonToolbar,
+  IonTitle,
+} from "@ionic/react";
+
+
 
 import { FC } from "react";
 
-export const AddEvent:FC<any> = ({isOpen, setIsOpen})=>{
+import { Camera, CameraResultType, CameraSource } from "@capacitor/camera";
+import { Storage } from "@ionic/storage";
 
-    return (
-      <>
-        <IonModal isOpen={isOpen}>
-          <IonHeader>
-            <IonToolbar>
-              <IonButtons slot="start">
-                <IonButton onClick={() => setIsOpen(false)}>Cancel</IonButton>
-              </IonButtons>
-              <IonTitle>Welcome</IonTitle>
-              <IonButtons slot="end">
-                <IonButton strong={true} onClick={() => confirm()}>
-                  Confirm
-                </IonButton>
-              </IonButtons>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className="ion-padding">
-            <IonItem>
-              <IonInput
-                label="Enter your name"
-                labelPlacement="stacked"
-                type="text"
-                placeholder="Your name"
-              />
-            </IonItem>
-          </IonContent>
-        </IonModal>
-      </>
-    );
-}
+export const AddEvent: FC<any> = ({ isOpen, setIsOpen }) => {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [audio, setAudio] = useState("");
+  const store = new Storage();
+
+
+  const confirm = async () => {
+    const eventData = {
+      title,
+      date,
+      description,
+      photo,
+    };
+
+    try {
+      store.create();
+      await store.set(
+        "eventData",
+        JSON.stringify(eventData),
+      );
+      console.log("Datos guardados exitosamente en el almacenamiento local.");
+    } catch (error) {
+      console.error("Error al guardar los datos:", error);
+    }
+
+    setIsOpen(false);
+  };
+
+  const takePhoto = async () => {
+    try {
+      const capturedPhoto = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera, // Utiliza la cámara para tomar la foto
+      });
+      setPhoto(capturedPhoto.webPath!);
+    } catch (error) {
+      console.error("Error al tomar la foto:", error);
+    }
+  };
+
+  return (
+    <>
+      <IonModal isOpen={isOpen}>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonButton onClick={() => setIsOpen(false)}>Cancel</IonButton>
+            </IonButtons>
+            <IonTitle>Add Event</IonTitle>
+            <IonButtons slot="end">
+              <IonButton strong={true} onClick={confirm}>
+                Confirm
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonItem>
+            <IonLabel position="stacked">Title</IonLabel>
+            <IonInput
+              type="text"
+              value={title}
+              onIonChange={(e) => setTitle(e.detail.value!)}
+            />
+          </IonItem>
+          <IonItem className="ion-padding-top">
+            <IonDatetime
+              value={date}
+              onIonChange={(e) =>
+                e.detail.value && setDate(e.detail.value.toString())
+              }
+            ></IonDatetime>
+          </IonItem>
+          <IonItem className="ion-padding-top">
+            <IonLabel position="stacked">Description</IonLabel>
+            <IonTextarea
+              value={description}
+              onIonChange={(e) => setDescription(e.detail.value!)}
+            />
+          </IonItem>
+          <IonItem className="ion-padding-top">
+            {photo !== "" && <IonImg src={photo} />}
+            <IonButton onClick={takePhoto}>Take Photo</IonButton>
+          </IonItem>
+          <IonItem className="ion-padding-top">
+            <audio controls src={audio} />
+            <input
+              type="file"
+              accept="audio/*"
+              onChange={(e) =>
+                setAudio(URL.createObjectURL(e.target.files![0]))
+              }
+            />
+          </IonItem>
+        </IonContent>
+      </IonModal>
+    </>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import {
   IonCol,
   IonContent,
@@ -10,13 +11,37 @@ import {
   IonFabButton,
   IonIcon,
   IonFabList,
+  IonImg,
 } from "@ionic/react";
-import { add} from "ionicons/icons";
-import { useState } from "react";
+import { add } from "ionicons/icons";
+import { Storage } from "@ionic/storage";
 import { AddEvent } from "../components/AddEvent";
 
 export const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [storedData, setStoredData] = useState<any>(null);
+  const store = new Storage();
+
+  useEffect(() => {
+    const getDataFromStorage = async () => {
+      try {
+        await store.create();
+        const result = await store.get("eventData");
+        if (result) {
+          // Verificar si result y result.value no son nulos
+          const eventData = JSON.parse(result);
+          setStoredData(eventData);
+          console.log(eventData);
+        }
+      } catch (error) {
+        console.error("Error al obtener los datos del storage:", error);
+      }
+    };
+    if (isOpen === false) {
+      getDataFromStorage();
+    }
+  }, [isOpen]); // La dependencia vacía [] asegura que el efecto se ejecute solo una vez al montar el componente
+
   return (
     <IonContent className="ion-padding">
       <IonGrid>
@@ -29,6 +54,17 @@ export const Home = () => {
           </IonCard>
         </IonCol>
       </IonGrid>
+      {storedData && ( // Mostrar los datos almacenados solo si hay datos disponibles
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>{storedData.title}</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonImg src={storedData.photo} alt="Imagen" />
+
+          </IonCardContent>
+        </IonCard>
+      )}
       <IonFab slot="fixed" vertical="bottom" horizontal="end" color="warning">
         <IonFabButton>
           <IonIcon icon={add} onClick={() => setIsOpen(true)}></IonIcon>
